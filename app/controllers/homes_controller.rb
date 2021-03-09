0d8fb09c567c3d99d9856332063f704991106009
EC2 Default User <ec2-user@ip-172-31-5-161.ec2.internal>
2021-03-09 09:10:35 +0000

    [add]

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,19 @@
 class HomesController < ApplicationController
    def top
    end
+   
+   def new
+      @book = Book.new
+   end 
+   
+   def create
+      book = Book.new(book_params)
+      book.save
+      redirect_to '/book'
+   end 
+   
+   private
+   def book_params
+      params.require(:list).permit(:title, :body)
+   end 
 end
