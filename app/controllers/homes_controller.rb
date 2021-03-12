8115c2e6f95d054fd25834223830e4b0105bc1fb
keita86 <ke4i2ta4@gmail.com>
2021-03-12 10:43:30 +0000

    [add]

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,18 +2,4 @@ class HomesController < ApplicationController
    def top
    end
    
-   def new
-      @book = Book.new
-   end 
-   
-   def create
-      book = Book.new(book_params)
-      book.save
-      redirect_to '/book'
-   end 
-   
-   private
-   def book_params
-      params.require(:book).permit(:title, :body)
-   end 
 end
